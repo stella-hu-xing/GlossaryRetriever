ef7e8c0b208f9a09503d4cfe8a12db3e4a250649
stella-hu <xinghu0164@gmail.com>
2021-11-19 22:02:15 +1100

    add handler

--- src/handler.ts
+++ src/handler.ts
@@ -1,7 +1,7 @@
 import { Context } from "aws-lambda";
 
-exports.handler = async function (event: any, context: Context) {
+export async function handler(event: any, context: Context) {
   console.log("Function name: ", context.functionName);
   console.log({ event, eventtime: Date.now().toLocaleString() });
   return context.functionName;
-};
+}
