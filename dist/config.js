786a76a21ac17065feaf38f760060ed67bf58dd5
stella-hu <xinghu0164@gmail.com>
2021-11-19 22:28:57 +1100

    add gitignore

--- dist/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = void 0;
-exports.config = {
-    accountName: "aHV4aW5nMDE2NEAxNjMuY29t",
-    accountPwd: "OTExMDI3LmNvbQ==",
-};
-//# sourceMappingURL=config.js.map
